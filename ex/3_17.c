b02d3e3cb0076530d76f0df6cd97142ea2393925
Alex Horlinsky <sashagorl@gmail.com>
2017-11-06 10:49:14 +0200

    allo2

--- ex/3_17.c
+++ ex/3_17.c
@@ -2,24 +2,35 @@
 
 int main(int argc, char const *argv[])
 {
-	unsigned int way;
+	int way;
 	int i = 1;
-	float zapravka, result, rashod, avgZapravka;
-
-	printf("Vvedite rashod (-1 for end): ");
-	scanf("%f", &rashod);
+	int check = 1;
+	float zapravka, rashod, avgZapravka;
+	float result = 0;
 	
-	while ( rashod != -1 )	{
-		printf("Пройденный путь: ");
-		scanf("%d", &way);
-		zapravka = (float) way / rashod;
-		printf("Nuzhno zapravit': %.2f\n", zapravka);
+	
+	
+	while ( check != 0 )	{
+		printf("Vvedite rashod (-1 for end): ");
+		scanf("%f", &rashod);
 		
-		result += zapravka;
-		i++;
+		if ( rashod = -1)
+			check = 0;
+		
+		while ( rashod != -1 )	{
+			printf("Пройденный путь: ");
+			scanf("%d", &way);
+		
+			zapravka = (float) way / rashod;
+		
+			printf("Nuzhno zapravit': %.2f\n\n", zapravka);
+		
+			result += zapravka;
+			i++;
+		}
 	}
 
-	avgZapravka = result / i;
+	avgZapravka = result / (float) i;
 	printf("Average chislo mile/galon: %.2f\n", avgZapravka);
 	
 	return 0;
